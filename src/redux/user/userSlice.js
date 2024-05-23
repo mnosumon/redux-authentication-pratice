71600f88b90e1f4bdf4f790d649a3d3542c11e74
Md Nuruddin Osman <mdnuruddin378350@gmail.com>
2024-05-23 16:00:43 +0600

    first commit

---
+++ src/redux/user/userSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    userInfo: null,
+}
+export const userSlice = createSlice({
+    name: 'userAuthentication',
+    initialState,
+    reducers: {
+        createUser: (state,action)=>{
+            state.userInfo = action.payload
+        }
+    }
+})
+export const {createUser} = userSlice.actions
+export default userSlice.reducer

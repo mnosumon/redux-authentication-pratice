e4f6aaf350fbd024883a1c1f9f5fe8ff1e418891
Md Nuruddin Osman <mdnuruddin378350@gmail.com>
2024-05-25 14:04:49 +0600

    react helmet async install and run and setup di=one

--- src/page/ragistration/Ragistration.jsx
+++ src/page/ragistration/Ragistration.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import RegisterDecoration from '../../components/registration/RegisterDecoration'
 import RegisterIcon from '../../components/registration/RegisterIcon'
 import RegisterFrom from '../../components/registration/RegisterFrom'
+import { Helmet } from 'react-helmet-async'
 
 const Ragistration = () => {
   return (
     <>
+    <Helmet>
+      <title>Registration</title>
+    </Helmet>
       <div className="relative">
         <div className="h-96 w-96 bg-primary rounded-full absolute -top-40 -left-40"></div>
         <div className="flex justify-center my-20 gap-8 container">
